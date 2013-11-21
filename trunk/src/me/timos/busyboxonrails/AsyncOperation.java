f8d6ff9c1750b159f727cce761e1a8bbb3f57e9d
thuanbkit@gmail.com <thuanbkit@gmail.com@f004ce48-d75c-4d74-fce8-71825f02d188>
2013-11-21 13:03:06 +0000

    Convert back end services to FragmentAsyncTasks Add AsyncBusyboxStatus to check System Busybox Status New GUI

--- trunk/src/me/timos/busyboxonrails/AsyncOperation.java
+++ trunk/src/me/timos/busyboxonrails/AsyncOperation.java
@@ -1,53 +1,36 @@
 package me.timos.busyboxonrails;
 
-import static me.timos.busyboxonrails.Constant.INTENT_OPERATION;
 import static me.timos.busyboxonrails.Utility.installBinary;
 
 import java.io.File;
 
 import me.timos.br.Logcat;
-import android.app.IntentService;
-import android.content.Intent;
 import android.widget.Toast;
 
-import com.stericson.RootTools.RootTools;
-
-public abstract class ServiceBase extends IntentService {
+public abstract class AsyncOperation extends
+		FragmentAsyncTask<Integer, Void, Void> {
 
 	protected SbApp mApp;
 	protected int mOpId;
 	private String mBusyboxResPath;
 
-	public ServiceBase(String name) {
-		super(name);
-	}
-
 	protected abstract void doBusybox(File busybox);
 
-	protected String getBusyboxResPath() {
-		return mBusyboxResPath;
-	}
-
 	@Override
-	protected void onHandleIntent(Intent intent) {
-		mOpId = intent.getIntExtra(INTENT_OPERATION, 0);
-		mApp = (SbApp) getApplication();
+	public Void doInBackground(Integer... params) {
+		mOpId = params[0];
+		mApp = (SbApp) getActivity().getApplication();
 
 		if (mOpId == 0) {
 			throw new IllegalStateException(
 					"Intent extra operation must be set");
 		}
 
-		if (!RootTools.isAccessGiven()) {
-			mApp.showToast(R.string.msg_root_na, Toast.LENGTH_LONG);
-			return;
-		}
-
 		String arch = System.getProperty("os.arch", "");
 		if (arch.startsWith("armv7")) {
 			Logcat.d("---armv7---");
 			mBusyboxResPath = "res/raw/busybox_armv7";
-			File busybox = installBinary(this, "busybox", R.raw.busybox_armv7,
+			File busybox = installBinary(mApp, "busybox", R.raw.busybox_armv7,
 					mBusyboxResPath);
 			if (busybox == null) {
 				mApp.showToast(R.string.error_write_binary_internal_data,
@@ -58,6 +41,11 @@ public abstract class ServiceBase extends IntentService {
 		} else {
 			mApp.showToast(R.string.error_unsupported_arch, Toast.LENGTH_LONG);
 		}
+		return null;
+	}
+
+	protected String getBusyboxResPath() {
+		return mBusyboxResPath;
 	}
 
 }
